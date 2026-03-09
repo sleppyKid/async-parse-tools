97565a5005d4372a56ded82fccd37a7a7fdcf553
Maxim <drotxx@yandex.ru>
2026-03-09 12:10:19 +0400

    playwright is optional

--- async_parse_tools/__init__.py
+++ async_parse_tools/__init__.py
@@ -1,5 +1,9 @@
 from .async_base import *
 from .async_downloader import AsyncDownloader, LengthError, FakeStringArray
-from .async_browser import AsyncBrowser, BrowserType, Page
 from .async_requests import AsyncRequests, ClientSession
 from .utils import *
+
+try:
+    from .async_browser import AsyncBrowser, BrowserType, Page
+except ImportError:
+    pass
